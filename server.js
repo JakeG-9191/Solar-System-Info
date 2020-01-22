47e3ac7e25052ccf6588b291db359dd326f5c7fc
JakeG-9191 <jacobgarlick@msn.com>
2020-01-22 15:17:02 -0700

    testing changes

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+const server = app.listen(PORT, () => `App listening on port ${PORT}...`);
+
+console.log('Testing');
+
+module.exports = server;
