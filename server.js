8af41f46dfa255fead9e53d8cd3f428e3744a909
JakeG-9191 <jacobgarlick@msn.com>
2020-01-23 15:07:28 -0700

    linked local database to begin building out new entries

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const users = require('./routes/users');
+const config = require('config');
+const mongoose = require('mongoose');
 
 const app = express();
 
@@ -7,6 +9,10 @@ app.use('/api/users', users);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true, useNewUrlParser: true }));
+
+const db = config.get('db');
+
+mongoose.connect(db).then(() => console.log(`Connected to ${db}`));
 
 const PORT = process.env.PORT || 3000;
 
