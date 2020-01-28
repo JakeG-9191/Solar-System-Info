6e77ab18865f344dabdb9bc13723544045edeb11
JakeG-9191 <jacobgarlick@msn.com>
2020-01-28 10:58:28 -0700

    post component now retrives and posts all current posts stored within the database

--- client/src/components/Posts/index.js
+++ client/src/components/Posts/index.js
@@ -9,16 +9,52 @@ class Posts extends Component {
   };
 
   componentDidMount() {
-    API.getPosts().then(data => {
-      console.log(data);
-    });
+    this.grabAllPosts();
   }
+
+  grabAllPosts = () => {
+    API.getPosts().then(res => {
+      console.log(res);
+      let myArray = [];
+      for (let i = 0; i < res.data.length; i++) {
+        let name = res.data[i].name;
+        let text = res.data[i].text;
+        let date = res.data[i].date;
+
+        let obj = {
+          name: name,
+          text: text,
+          date: date
+        };
+
+        myArray.push(obj);
+      }
+
+      this.setState({
+        posts: myArray
+      });
+
+      console.log(this.state.posts);
+    });
+  };
 
   render() {
     return (
       <div>
+        <Link to='/'>
+          <button className='btn btn-success'>Home</button>
+        </Link>
         <h1>Testing Posts</h1>
-        <h2>{this.state.posts}</h2>
+        <button onClick={this.grabAllPosts}>Grab Posts</button>
+        <h2>
+          {this.state.posts.map(post => (
+            <div id='all-posts'>
+              <h4>Name: {post.name}</h4>
+              <h4>Comment: {post.text}</h4>
+              <h4>Date: {post.date}</h4>
+            </div>
+          ))}
+        </h2>
       </div>
     );
   }
