f2019041c8d97a4dec25dc172f0e8e0026045f05
JakeG-9191 <jacobgarlick@msn.com>
2020-02-03 17:31:54 -0700

    began to build out quiz game items under game section of website

--- client/src/components/Game/index.js
+++ client/src/components/Game/index.js
@@ -4,11 +4,44 @@ import { Link } from 'react-router-dom';
 import './style.css';
 
 class Game extends Component {
-  state = {};
+  state = {
+    gameEdition: '',
+    gameStarted: false
+  };
 
-  // componentDidMount() {
-  //     API.getUsers()
-  // }
+  startGame = () => {
+    if (this.state.gameEdition === 'Earth') {
+      console.log('Testing');
+    } else if (this.state.gameEdition === 'Solar System') {
+      console.log('Test For Solar System');
+    } else if (this.state.gameEdition === 'Classified') {
+      console.log('Classified Edition');
+    }
+  };
+
+  earthEdition = e => {
+    e.preventDefault();
+    this.setState({
+      gameEdition: 'Earth',
+      gameStarted: true
+    });
+  };
+
+  solarEdition = e => {
+    e.preventDefault();
+    this.setState({
+      gameEdition: 'Solar System',
+      gameStarted: true
+    });
+  };
+
+  classifiedEdition = e => {
+    e.preventDefault();
+    this.setState({
+      gameEdition: 'Classified',
+      gameStarted: true
+    });
+  };
 
   render() {
     return (
@@ -34,6 +67,12 @@ class Game extends Component {
         </div>
         <div>
           <h1>Testing Game</h1>
+          <h3>Game Directions to go here</h3>
+          <button onClick={this.earthEdition}>Earth Edition</button>
+          <button onClick={this.solarEdition}>
+            General Solar System Edition
+          </button>
+          <button onClick={this.classifiedEdition}>Classified Edition</button>
         </div>
       </>
     );
