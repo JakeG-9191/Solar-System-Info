d15eaf70a8ec9656bc903230e61bc3f69ed87bbd
JakeG-9191 <jacobgarlick@msn.com>
2020-02-28 17:26:51 -0700

    added photo generation depending on user search parameters, will currenlty display up to 8 photos based on parameters

--- client/src/components/Mars/index.js
+++ client/src/components/Mars/index.js
@@ -4,6 +4,7 @@ import Select from 'react-select';
 import API from '../../utils/API';
 import './style.css';
 
+// defines all options available for users to search when looking at cameras
 const options = [
   { value: 'fhaz', label: 'Front Hazard Avoidance Camera' },
   { value: 'rhaz', label: 'Rear Hazard Avoidance Camera' },
@@ -19,6 +20,7 @@ const options = [
   }
 ];
 
+// customizes style of dropdown to select cameras
 const customStyles = {
   container: provided => ({
     ...provided,
@@ -34,7 +36,8 @@ const customStyles = {
   }),
   control: () => ({
     // none of react-select's styles are passed to <Control />
-    width: 200
+    width: 200,
+    color: 'white'
   }),
   singleValue: (provided, state) => {
     const opacity = state.isDisabled ? 1 : 1;
@@ -50,7 +53,9 @@ class Mars extends Component {
     userDateInput: '',
     userCameraInput: null,
     marsUpdatedWeather: [],
-    marsUpdatedSols: []
+    marsUpdatedSols: [],
+    roverName: [],
+    roverPhoto: []
   };
 
   handleChange = userCameraInput => {
@@ -107,12 +112,36 @@ class Mars extends Component {
     });
   };
 
+  // grabs available photos once user has selected a date and camera angle to search
   getRoverPhotos = e => {
     e.preventDefault();
     let camera = this.state.userCameraInput.value;
     let date = this.state.userDateInput;
     API.getMarsPhotos(date, camera).then(data => {
-      console.log(data.data);
+      // console.log(data.data);
+      // console.log(data.data.photos);
+      // console.log(data.data.photos.length);
+      // console.log(data.data.photos[0].camera.name);
+      // console.log(data.data.photos[0].img_src);
+
+      let results = data.data;
+      let totalPhotos = results.photos.length;
+      let cameraName = [];
+      let imgSource = [];
+      for (let i = 0; i < totalPhotos; i++) {
+        let singleName = results.photos[i].camera.name;
+        let singleImg = results.photos[i].img_src;
+        cameraName.push(singleName);
+        imgSource.push(singleImg);
+      }
+      if (cameraName.length > 0) {
+        this.setState({
+          roverName: cameraName,
+          roverPhoto: imgSource
+        });
+      }
+      console.log(this.state.roverName);
+      console.log(this.state.roverPhoto);
     });
   };
 
@@ -158,25 +187,44 @@ class Mars extends Component {
           <h1>The Mars Special</h1>
           <div className='container'>
             <div className='row'>
-              <div className='nasa-weather col-md-5'>
-                <iframe
-                  src='https://mars.nasa.gov/layout/embed/image/insightweather/'
-                  width='800'
-                  height='530'
-                  scrolling='no'
-                  frameBorder='0'
-                ></iframe>
+              <div className='martian-weather col-md-6'>
+                <h5>
+                  {this.state.marsUpdatedSols[0]
+                    ? `Sol ${this.state.marsUpdatedSols[0]} | Average Air Temp: ${this.state.marsUpdatedWeather[0]} F`
+                    : 'No Data Available For This Sol'}
+                </h5>
+                <h5>
+                  {this.state.marsUpdatedSols[1]
+                    ? `Sol ${this.state.marsUpdatedSols[1]} | Average Air Temp: ${this.state.marsUpdatedWeather[1]} F`
+                    : 'No Data Available For This Sol'}
+                </h5>
+                <h5>
+                  {this.state.marsUpdatedSols[2]
+                    ? `Sol ${this.state.marsUpdatedSols[2]} | Average Air Temp: ${this.state.marsUpdatedWeather[2]} F`
+                    : 'No Data Available For This Sol'}
+                </h5>
+                <h5>
+                  {this.state.marsUpdatedSols[3]
+                    ? `Sol ${this.state.marsUpdatedSols[3]} | Average Air Temp: ${this.state.marsUpdatedWeather[3]} F`
+                    : 'No Data Available For This Sol'}
+                </h5>
+                <h5>
+                  {this.state.marsUpdatedSols[4]
+                    ? `Sol ${this.state.marsUpdatedSols[4]} | Average Air Temp: ${this.state.marsUpdatedWeather[4]} F`
+                    : 'No Data Available For This Sol'}
+                </h5>
+                <h5>
+                  {this.state.marsUpdatedSols[5]
+                    ? `Sol ${this.state.marsUpdatedSols[5]} | Average Air Temp: ${this.state.marsUpdatedWeather[5]} F`
+                    : 'No Data Available For This Sol'}
+                </h5>
+                <h5>
+                  {this.state.marsUpdatedSols[6]
+                    ? `Sol ${this.state.marsUpdatedSols[6]} | Average Air Temp: ${this.state.marsUpdatedWeather[6]} F`
+                    : 'No Data Available For This Sol'}
+                </h5>
               </div>
-              <div className='martian-weather col-md-3'>
-                <h5>{`Sol ${this.state.marsUpdatedSols[0]} | Average Air Temp: ${this.state.marsUpdatedWeather[0]}`}</h5>
-                <h5>{`Sol ${this.state.marsUpdatedSols[1]} | Average Air Temp: ${this.state.marsUpdatedWeather[1]}`}</h5>
-                <h5>{`Sol ${this.state.marsUpdatedSols[2]} | Average Air Temp: ${this.state.marsUpdatedWeather[2]}`}</h5>
-                <h5>{`Sol ${this.state.marsUpdatedSols[3]} | Average Air Temp: ${this.state.marsUpdatedWeather[3]}`}</h5>
-                <h5>{`Sol ${this.state.marsUpdatedSols[4]} | Average Air Temp: ${this.state.marsUpdatedWeather[4]}`}</h5>
-                <h5>{`Sol ${this.state.marsUpdatedSols[5]} | Average Air Temp: ${this.state.marsUpdatedWeather[5]}`}</h5>
-                <h5>{`Sol ${this.state.marsUpdatedSols[6]} | Average Air Temp: ${this.state.marsUpdatedWeather[6]}`}</h5>
-              </div>
-              <div className='col-md-4'>
+              <div className='col-md-6'>
                 <form>
                   <label>
                     Input Earth Date:
@@ -206,6 +254,19 @@ class Mars extends Component {
                     Get Mars Photos
                   </button>
                 </div>
+              </div>
+              <div className='col-md-12'>
+                <h3>{this.state.roverName[0]}</h3>
+                <img className='rover-image' src={this.state.roverPhoto[0]} />
+                <img className='rover-image' src={this.state.roverPhoto[1]} />
+                <img className='rover-image' src={this.state.roverPhoto[2]} />
+                <img className='rover-image' src={this.state.roverPhoto[3]} />
+              </div>
+              <div className='col-md-12'>
+                <img className='rover-image' src={this.state.roverPhoto[4]} />
+                <img className='rover-image' src={this.state.roverPhoto[5]} />
+                <img className='rover-image' src={this.state.roverPhoto[6]} />
+                <img className='rover-image' src={this.state.roverPhoto[7]} />
               </div>
             </div>
           </div>
