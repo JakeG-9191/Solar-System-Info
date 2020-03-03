cc015eae178e45e7ea980d494dbf6caee5457c67
JakeG-9191 <jacobgarlick@msn.com>
2020-03-02 17:27:41 -0700

    began building out new interactive map, need to add coordinates to all items that should be highlighted

--- client/src/components/SpaceMap/index.js
+++ client/src/components/SpaceMap/index.js
@@ -1,11 +1,63 @@
 import React, { Component } from 'react';
 import API from '../../utils/API';
+import ImageTest from '../../utils/spaceMap/0.jpg';
+import ImageMapper from 'react-image-mapper';
 import { Link } from 'react-router-dom';
 import './style.css';
 
+const URL = ImageTest;
+const MAP = {
+  name: 'my-map',
+  areas: [
+    {
+      name: '1',
+      shape: 'poly',
+      coords: [25, 33, 27, 300, 128, 240, 128, 94],
+      preFillColor: 'green',
+      fillColor: 'blue'
+    },
+    {
+      name: '2',
+      shape: 'poly',
+      coords: [219, 118, 220, 210, 283, 210, 284, 119],
+      preFillColor: 'pink'
+    },
+    {
+      name: '3',
+      shape: 'poly',
+      coords: [381, 241, 383, 94, 462, 53, 457, 282],
+      fillColor: 'yellow'
+    },
+    {
+      name: '4',
+      shape: 'poly',
+      coords: [245, 285, 290, 285, 274, 239, 249, 238],
+      preFillColor: 'red'
+    },
+    { name: '5', shape: 'circle', coords: [335, 975, 50] },
+    { name: '6', shape: 'circle', coords: [380, 875, 50] }
+  ]
+};
+
 class SpaceMap extends Component {
+  state = {
+    hoveredArea: null
+  };
+
   componentDidMount() {
     this.loadBackground();
+  }
+
+  enterArea(area) {
+    this.setState({ hoveredArea: area });
+  }
+
+  leaveArea(area) {
+    this.setState({ hoveredArea: null });
+  }
+
+  getTipPosition(area) {
+    return { top: `${area.center[1]}px`, left: `${area.center[0]}px` };
   }
 
   loadBackground = () => {
@@ -43,7 +95,22 @@ class SpaceMap extends Component {
             </button>
           </Link>
         </div>
-        <div>
+        <div className='container'>
+          <ImageMapper
+            src={URL}
+            map={MAP}
+            width={2200}
+            onMouseEnter={area => this.enterArea(area)}
+            onMouseLeave={area => this.leaveArea(area)}
+          />
+          {this.state.hoveredArea && (
+            <span
+              className='tooltip'
+              style={{ ...this.getTipPosition(this.state.hoveredArea) }}
+            >
+              {this.state.hoveredArea && this.state.hoveredArea.name}
+            </span>
+          )}
           <h1>Testing Interactive Map</h1>
           <ul>
             <li>Several Interactive Elements, should be clicked on</li>
