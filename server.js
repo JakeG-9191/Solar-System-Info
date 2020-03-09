08017751c70a2fff383f0badccd7de55d93ad0b3
JakeG-9191 <jacobgarlick@msn.com>
2020-03-08 21:34:45 -0600

    updated scripts server side to attempt to resolve deployment issue

--- server.js
+++ server.js
@@ -9,9 +9,11 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true, useNewUrlParser: true }));
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
-}
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build/index.html'));
+});
 
 app.use('/api/users', users);
 app.use('/api/posts', posts);
