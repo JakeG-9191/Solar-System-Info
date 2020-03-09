0ce1981151af2346f2e294e1406e6c1dd39a078c
JakeG-9191 <jacobgarlick@msn.com>
2020-03-09 14:27:31 -0600

    added path requirement, updated production requirements on server

--- server.js
+++ server.js
@@ -3,17 +3,22 @@ const mongoose = require('mongoose');
 const users = require('./routes/users');
 const posts = require('./routes/posts');
 const config = require('config');
+const path = require('path');
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true, useNewUrlParser: true }));
 
-app.use(express.static(path.join(__dirname, 'build')));
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build/index.html'));
-});
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(_dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 app.use('/api/users', users);
 app.use('/api/posts', posts);
