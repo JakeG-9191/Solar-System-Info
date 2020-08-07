0af09821eb51c8bd5049c4cc793b95f00b185c86
JakeG-9191 <jacobgarlick@msn.com>
2020-08-07 17:24:13 -0600

    updated res sendfile to test crash on heroku when client reloads on any page but home

--- server.js
+++ server.js
@@ -16,7 +16,8 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 
   app.get('/*', (req, res) => {
-    res.sendFile(path.resolve(_dirname, 'client', 'build', 'index.html'));
+    // res.sendFile(path.resolve(_dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, '../public', 'index.html'));
   });
 }
 
